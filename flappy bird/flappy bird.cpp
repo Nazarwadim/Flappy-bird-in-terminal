f3a019729b479c436f82c24cbbb95290fe6e5072
NazarVadim <nazarii.yablonskyi.pp.2022@ipnu.ua>
2023-02-09 15:13:41 +0200

    some message

--- flappy bird/flappy bird.cpp
+++ flappy bird/flappy bird.cpp
@@ -3,7 +3,8 @@
 #include <conio.h>
 #include <time.h>
 #include "../Header1.h"
-#pragma comment(lib, "winmm.lib")//povezlo-povezlo
+#pragma comment(lib, "winmm.lib")
+
 class Bird {
 private:
     float anim_time = 0;
@@ -21,8 +22,8 @@ public:
     {
         this->anim_time = anim_time;
     }
-    void render_bird(Resl &display)
-    {   
+    void render_bird(Resl& display)
+    {
         if (anim_time > 2) {
             anim_speed = -0.25;
         }
@@ -32,12 +33,12 @@ public:
         draw_pixel(display, o.get_x() - 1, o.get_y(), 9);
         draw_pixel(display, o.get_x() - 2, o.get_y(), 9);
         draw_pixel(display, o.get_x(), o.get_y() + (int)anim_time - 1, 6); // wing 
-        anim_time+=anim_speed;
+        anim_time += anim_speed;
     }
     void set_Phisics()
     {
         float g = 9.8;
-        speedY += g /180;
+        speedY += g / 140;
         float temp_y = o.get_y();
         temp_y += speedY;
         o.set_y(temp_y);
@@ -46,7 +47,7 @@ public:
 void generate_sth(Resl* display, Point peregoroda[], int n);
 void set_peregoroga_x(Point peregoroda[], float value, int number_of_element);
 void draw_peregoroda(Point peregoroda[], int number_of_element, Resl* display);
-void draw_peregoroda(Point peregoroda[], int number_of_element, Resl &display);
+void draw_peregoroda(Point peregoroda[], int number_of_element, Resl& display);
 void check_key(char* key, Bird* bird)
 {
     int point_on_display = 0;
@@ -57,11 +58,11 @@ void check_key(char* key, Bird* bird)
         switch (*key)
         {
         case ' ':
-            PlaySound(TEXT("arkadnaya-igra-power-power-sound-41566.wav"), NULL, SND_FILENAME | SND_ASYNC);
             //move up
+            PlaySound(TEXT("arkadnaya-igra-power-power-sound-41566.wav"), NULL, SND_FILENAME | SND_ASYNC);
             bird->time = 0;
             bird->set_anim_time(3);
-            bird->speedY = -1;
+            bird->speedY = -0.8;
             break;
         }
     }
@@ -77,7 +78,7 @@ int main()
     srand(time(NULL));
     char key;
     Resl display;
-    Bird bird(15, 10);    
+    Bird bird(15, 10);
     int time = -1000;
     display.cleardisplay();
 
@@ -85,12 +86,12 @@ int main()
     for (int i = 0; i < 1000; i++) {
         display.cleardisplay();
         char c = light == true ? 177 : ' ';
-        if(!(i % 50))light = !light;
-        
+        if (!(i % 50))light = !light;
+
         memset(display.display, c, R - 1);
         display.setdisplay();
         puts(display.display);
-       
+
     }
 
     //update
@@ -102,7 +103,7 @@ int main()
     std::thread t(check_key, &key, &bird);
     for (float i = display.resx, m = display.resx + 40, l = display.resx + 80;; )
     {
-        if (i < 0 ) {
+        if (i < 0) {
             i = display.resx;
             generate_sth(&display, peregoroda, 0);
         }
@@ -114,15 +115,15 @@ int main()
             l = display.resx;
             generate_sth(&display, peregoroda, 2);
         }
-        if (score < 20) { i -= 0.2; m -= 0.2; l -= 0.2; }
-        else { i -= 0.5; m -= 0.5; l -= 0.5;}         //speed of blocks
- 
+        if (score < 20) { i -= 0.5; m -= 0.5; l -= 0.5; }
+        else { i -= 1.5; m -= 1.5; l -= 1.5; }         //speed of blocks
+
         short frame = 1000 / (clock() - time + 1);
         time = clock();
         display.cleardisplay();
         bird.render_bird(display);
         bird.set_Phisics();
-        
+
         set_peregoroga_x(peregoroda, i, 0);
         set_peregoroga_x(peregoroda, m, 1);
         set_peregoroga_x(peregoroda, l, 2);
@@ -137,14 +138,16 @@ int main()
         int point_on_display = (int)bird.o.get_x() + (int)bird.o.get_y() * display.resx;
         if (bird.o.get_y() > display.resy || bird.o.get_y() < 0 || display.display[point_on_display] == '$')
         {
-            PlaySound(TEXT("pzdyi - dal.wav"), NULL, SND_FILENAME | SND_ASYNC);
             std::cout << "You die" << '\n';
+            PlaySound(TEXT("pzdyi-dal.wav"), NULL, SND_FILENAME | SND_ASYNC);
+            
             std::cout << "Play again? Y/N  (double tap) or P to pay 10492348237409$ to continue" << '\n';
 
             char c = getchar();
             fflush(stdin);
             if (c == 'p') {
-                bird.o.set_y(10);
+                PlaySound(TEXT("povezlo-povezlo.wav"), NULL, SND_ASYNC);
+                bird.o.set_y(3);
                 bird.speedY = 0;
                 i--;
                 m--;
@@ -163,29 +166,26 @@ int main()
 
         for (int j = 0; j < 12; j += 4)
         {
-           // std::cout << (int)peregoroda[j].get_x() << ' ';
-            if ((int)peregoroda[j].get_x()  == 14)
+            // std::cout << (int)peregoroda[j].get_x() << ' ';
+            if ((int)peregoroda[j].get_x() == 14)
             {
-                PlaySound(TEXT("povezlo-povezlo.wav"), NULL, SND_FILENAME | SND_ASYNC);
                 score++;
-                break;
-            }     
+            }
         }
-        printf("\n");
         display.setdisplay();
-       // std::cout <<'\n' << "FPS: " << frame << '\n' << "Score: " << score << '\n';
+        //std::cout <<'\n' << "FPS: " << frame << '\n' << "Score: " << score<<"\n";
         puts(display.display);
-        Sleep(20);  // 60 frames per sec // 30 for 30 fps     50 for 20
+        Sleep(20);  // 60 frames per sec // 20 for 30 fps     50 for 20
     }
     t.detach();
     return 0;
-} 
+}
 void generate_sth(Resl* display, Point peregoroda[], int n)
 {
-    float y2[] = { 0,33 };
+    float y2[] = { 0,(float)display->resy };
     bool up = rand() % 2;
-    float y1 = rand() % (display->resy - 10);
-    float ytd = y1 > 10 ? y1 : y1 + 10;
+    float y1 = rand() % (display->resy -8);
+    float ytd = y1 > 10 ? y1 : y1 + 8;
     Point A(display->resx - 11, ytd);
     Point B(display->resx - 11, y2[up]);
     Point C(display->resx - 1, y2[up]);
@@ -195,21 +195,21 @@ void generate_sth(Resl* display, Point peregoroda[], int n)
     peregoroda[4 * n + 2] = C;
     peregoroda[4 * n + 3] = D;
 }
-void set_peregoroga_x(Point peregoroda[],float value, int number_of_element)
+void set_peregoroga_x(Point peregoroda[], float value, int number_of_element)
 {
     float dx = peregoroda[4 * number_of_element + 2].get_x() - peregoroda[4 * number_of_element].get_x();
     for (int i = number_of_element * 4; i < number_of_element * 4 + 2; i++)
     {
-        peregoroda[i].set_x(value - dx/2);
+        peregoroda[i].set_x(value - dx / 2);
     }
     for (int i = number_of_element * 4 + 2; i < number_of_element * 4 + 4; i++)
     {
         peregoroda[i].set_x(value + dx / 2);
     }
 }
-void draw_peregoroda(Point peregoroda[],int number_of_element, Resl &display)
+void draw_peregoroda(Point peregoroda[], int number_of_element, Resl& display)
 {
-    drawline(peregoroda[4*number_of_element+0], peregoroda[4 * number_of_element + 1], display);
+    drawline(peregoroda[4 * number_of_element + 0], peregoroda[4 * number_of_element + 1], display);
     drawline(peregoroda[4 * number_of_element + 2], peregoroda[4 * number_of_element + 3], display);
     drawline(peregoroda[4 * number_of_element + 3], peregoroda[4 * number_of_element + 0], display);
 }
